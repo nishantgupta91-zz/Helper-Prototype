d20c7edcef9ac00a7e50bd83d063abf73176d88e
nishantgupta91 <Nishant@Nishant-PC.mshome.net>
2015-08-31 01:06:20 +0200

    video frames painted on canvas

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,10 +3,35 @@
  */
 angular.module('mainApp')
 
-    .controller('mainController', ['$scope', 'video', function($scope, video, $q){
+    .controller('mainController', ['$scope', 'video', function($scope, video){
         video.addSource('mp4', 'app/resources/hand.mp4');
         $scope.cursorIcon = "";
         console.log('==== main ====');
+        $scope.drawCanvas = function() {
+            if (window.requestAnimationFrame) window.requestAnimationFrame($scope.drawCanvas);
+            // IE implementation
+            else if (window.msRequestAnimationFrame) window.msRequestAnimationFrame($scope.drawCanvas);
+            // Firefox implementation
+            else if (window.mozRequestAnimationFrame) window.mozRequestAnimationFrame($scope.drawCanvas);
+            // Chrome implementation
+            else if (window.webkitRequestAnimationFrame) window.webkitRequestAnimationFrame($scope.drawCanvas);
+            // Other browsers that do not yet support feature
+            else setTimeout($scope.drawCanvas, 16.7);
+            $scope.drawVideoOnCanvas();
+        };
+        $scope.drawVideoOnCanvas = function() {
+            var backgroundObject = document.getElementById("videoBackgrounddata");
+            var canvas = document.getElementById("outputCanvas");
+            var width = canvas.width;
+            var height = canvas.height;
+            if (canvas.getContext) {
+                var context = canvas.getContext('2d');
+                context.drawImage(backgroundObject, 0, 0, width, height);
+                //var imgBackgroundData = context.getImageData(0, 0, width, height);
+                //var imgData = context.createImageData(width, height);
+                //context.putImageData(imgData, 0, 0);
+            }
+        };
     }])
 
     .controller('toolboxController', function($scope, $mdBottomSheet) {
