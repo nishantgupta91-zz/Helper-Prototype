fa9c0756dcffdce1b3cb87fe8e764b8526602a41
nishantgupta91 <nishant8568@gmail.com>
2015-10-05 10:04:19 +0200

    grunt configurations changed

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,29 +6,73 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
+        autoprefixer: {
+            build: {
+                expand: true,
+                src: [ 'app/css/**/*.css' ],
+                dest: 'build'
+            }
+        },
+        cssmin: {
+            build: {
+                files: {
+                    'build/application.css': [ 'build/**/*.css' ]
+                }
+            }
+        },
         concat: {
             options: {
                 separator: ';'
             },
-            dist: {
-                src: ['app/js/app.js', 'app/js/controllers.js', 'app/js/directives.js', 'app/js/filters.js', 'app/js/services.js'],
+            js: {
+                src: ['vendor/js/**/*.js', 'app/js/**/*.js'],
                 dest: 'build/script.js'
+            },
+            css: {
+                src: ['vendor/css/**/*.css', 'app/css/**/*.css'],
+                dest: 'build/style.css'
             }
         },
         uglify: {
             options: {
-
+                // the banner is inserted at the top of the output
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
             },
             build: {
                 src: 'build/script.js',
                 dest: 'build/script.min.js'
             }
+        },
+        watch: {
+            stylesheets: {
+                files: 'app/css/**/*.css',
+                tasks: [ 'stylesheets' ]
+            },
+            scripts: {
+                files: 'app/js/**/*.js',
+                tasks: [ 'scripts' ]
+            }
+        },
+        useminPrepare: {
+            html: 'index.html'
         }
     });
-    // Load the plugin that provides the "uglify" task.
+
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-usemin');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
+    grunt.registerTask(
+        'stylesheets',
+        [ 'autoprefixer', 'cssmin' ]
+    );
+    grunt.registerTask(
+        'scripts',
+        [ 'concat', 'uglify' ]
+    );
     // Default task(s).
-    grunt.registerTask('default', ['concat', 'uglify']);
+    grunt.registerTask('default', ['stylesheets', 'scripts']);
 };
